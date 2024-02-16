c76d330289857e10ea0a6054daeb93d819e85ccc
cballevre <celestin.ballevre@protonmail.com>
2024-02-16 10:52:14 +0100

    feat: Add analytics

--- components/Layout.js
+++ components/Layout.js
@@ -13,6 +13,7 @@ export default function Layout({ children }) {
         <meta name="Description" content=""></meta>
         <title>Célestin Ballèvre</title>
         <link rel="shortcut icon" href={prefix + '/favicon.ico'} />
+        <script async src="https://eu.umami.is/script.js" data-website-id="ca8a6ca0-048f-457c-a3df-b1505e35414d"></script>
       </Head>
       <main className="mx-auto max-w-2xl p-8 md:px-0">
         <Header />
